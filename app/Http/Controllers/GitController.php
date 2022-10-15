f3e08ee7e841347b8a12966fcc270e7ef708f4c0
dzhando <107620912+dzhando@users.noreply.github.com>
2022-10-15 11:26:55 +0600

    Git controller testovyi

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class GitController extends Controller
+class AppController extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
